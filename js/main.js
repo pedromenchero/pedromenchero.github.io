00ac72387566e4fd4fce36cc2e0a450c3e77f942
Pedro <pedro.menchero@gmail.com>
2018-03-08 18:23:03 +0100

    Update
    Update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -22,25 +22,29 @@ $('#nav').affix({
       offset: {
         top: $('header').height()
       }
-});	
+});
 
 	// skills chart
 	$(document).ready(function(e) {
 	//var windowBottom = $(window).height();
 	var index=0;
 	$(document).scroll(function(){
-		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
-		if(top<-300){
-			if(index==0){	
-			
+		var top = $('#about').outerHeight()+$(window).scrollTop();
+    var skills = $('#skills-circles').position().top;
+    // console.log("About > " + $('#about').height());
+    // console.log("Skills height > " + $('#skills-circles').height());
+    // console.log("Skills top > " + $('#skills-circles').position().top);
+    // console.log("Scroll Top > " + $(window).scrollTop());
+		// console.log("Top > " + top);
+		if(top >= skills * 1.05){
+			if(index==0){
 				$('.chart').easyPieChart({
 					easing: 'easeOutBounce',
 					onStep: function(from, to, percent) {
 						$(this.el).find('.percent').text(Math.round(percent));
 					}
 				});
-			
+
 				}
 			index++;
 		}
@@ -76,22 +80,22 @@ $('#nav').affix({
         });
 
     });
-	
-	  	
+
+
     // CounterUp
 	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
+		if($("span.count").length > 0){
 			$('span.count').counterUp({
 					delay: 10, // the delay time in ms
 			time: 1500 // the speed time in ms
 			});
 		}
 	});
-	
+
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 
 }());
 
