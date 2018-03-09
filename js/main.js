13446eb8fce22620c50af843897f63b83d3ef413
Pedro Menchero <pedro.menchero@gmail.com>
2018-03-09 11:31:31 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,122 +1,120 @@
-
 function main() {
 
-(function () {
-   'use strict';
+  (function() {
+    'use strict';
 
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
+    $('a.page-scroll').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 40
+          }, 900);
+          return false;
         }
-      });
+      }
+    });
 
-	// affix the navbar after scroll below header
-$('#nav').affix({
+    // affix the navbar after scroll below header
+    $('#nav').affix({
       offset: {
         top: $('header').height()
       }
-});
+    });
 
-	// skills chart
-	$(document).ready(function(e) {
-  	//var windowBottom = $(window).height();
-  	var index=0;
+    // skills chart
+    $(document).ready(function(e) {
+      //var windowBottom = $(window).height();
+      var index = 0;
 
-    // console.log($(window).height());
-    // console.log($('#skills-circles').position().top);
+      // console.log($(window).height());
+      // console.log($('#skills-circles').position().top);
 
-    if ($(window).height() > $('#skills-circles').position().top) {
-      if(index==0){
-        $('.chart').easyPieChart({
-          easing: 'easeOutBounce',
-          barColor: '#d1232b',
-          onStep: function(from, to, percent) {
-            $(this.el).find('.percent').text(Math.round(percent));
+      if ($(window).height() > $('#skills-circles').position().top) {
+        if (index == 0) {
+          $('.chart').easyPieChart({
+            easing: 'easeOutBounce',
+            barColor: '#629ca7',
+            onStep: function(from, to, percent) {
+              $(this.el).find('.percent').text(Math.round(percent));
+            }
+          });
+          index++;
+        }
+      }
+
+      $(document).scroll(function() {
+        var top = $('#about').outerHeight() + $(window).scrollTop();
+        var skills = $('#skills-circles').position().top;
+        // console.log("About > " + $('#about').height());
+        // console.log("Skills height > " + $('#skills-circles').height());
+        // console.log("Skills top > " + $('#skills-circles').position().top);
+        // console.log("Scroll Top > " + $(window).scrollTop());
+        // console.log("Top > " + top);
+        if (top >= skills * 1.05) {
+          if (index == 0) {
+            $('.chart').easyPieChart({
+              easing: 'easeOutBounce',
+              barColor: '#629ca7',
+              onStep: function(from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent));
+              }
+            });
+
+          }
+          index++;
+        }
+      })
+      //console.log(nagativeValue)
+    });
+
+
+    // Portfolio isotope filter
+    $(window).load(function() {
+      var $container = $('.portfolio-items');
+      $container.isotope({
+        filter: '*',
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      });
+      $('.cat a').click(function() {
+        $('.cat .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+          filter: selector,
+          animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
           }
         });
-
-        }
-        index++
-    }
-
-  	$(document).scroll(function(){
-  		var top = $('#about').outerHeight()+$(window).scrollTop();
-      var skills = $('#skills-circles').position().top;
-      // console.log("About > " + $('#about').height());
-      // console.log("Skills height > " + $('#skills-circles').height());
-      // console.log("Skills top > " + $('#skills-circles').position().top);
-      // console.log("Scroll Top > " + $(window).scrollTop());
-  		// console.log("Top > " + top);
-  		if(top >= skills * 1.05){
-  			if(index==0){
-  				$('.chart').easyPieChart({
-  					easing: 'easeOutBounce',
-            barColor: '#d1232b',
-  					onStep: function(from, to, percent) {
-  						$(this.el).find('.percent').text(Math.round(percent));
-  					}
-  				});
-
-  				}
-  			  index++;
-  		}
-  	})
-  	//console.log(nagativeValue)
-	});
-
-
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
+        return false;
+      });
 
     });
 
 
     // CounterUp
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1500 // the speed time in ms
-			});
-		}
-	});
+    $(document).ready(function($) {
+      if ($("span.count").length > 0) {
+        $('span.count').counterUp({
+          delay: 10, // the delay time in ms
+          time: 1500 // the speed time in ms
+        });
+      }
+    });
 
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
+    // Pretty Photo
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+      social_tools: false
+    });
 
-}());
+  }());
 
 
 }
